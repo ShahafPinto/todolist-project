800d72c506de46f293a25a4afcc9798b31d029c7
ShahafPinto <shahafchen7@gmail.com>
2022-11-30 11:07:20 +0200

    ..

--- src/application.js
+++ src/application.js
@@ -14,7 +14,7 @@ function render(){
 let todoList = [];
 let task='';
 let counter = document.querySelector('.todo-count').getElementsByTagName('strong')[0];
-
+// let state = false; 
 
 function build(_event){
     if (_event.key === 'Enter'){
@@ -30,8 +30,6 @@ function renderDisplay(){
     task = '';
     inputTask.value = '';
     counter.innerHTML = todoList.length;
-    // delarr();
-    
 }
 
 function addTask(){
@@ -52,27 +50,30 @@ function addTask(){
         newLi.setAttribute('id','li'+String(todoList.length));
     }
     todoList.push(task);
-    del();
+    addDelLiseten();
     renderDisplay();
 }
 
 const inputTask = document.querySelector('.new-todo');
-// inputTask.addEventListener('Enter',todoTask.addNewTask(inputTask));
 inputTask.addEventListener('keyup', build);
 
 
 
-function del(){
-    let del = Array.from(document.querySelectorAll('.destroy')); 
-    del[del.length-1].addEventListener('click', (e) =>  delTask(e) );
+function addDelLiseten(){
+        let del = Array.from(document.querySelectorAll('.destroy')); 
+        del[del.length-1].addEventListener('click', (e) =>  delTask(e) ); 
 } 
 
 
 
 function delTask(e){
-    console.log('press on X');
-    console.log(e.target.id);
-    document.querySelector(`#li${e.target.id}`).remove();
+    if (todoList.length==1){
+        document.querySelector('.todo-list').classList.add('hidden');
+        document.querySelector('.footer').classList.add('hidden');
+        document.querySelector('.toggle-all').classList.add('hidden');
+    }else{
+        document.querySelector(`#li${e.target.id}`).remove();
+    } 
     indexForRemoval = e.target.id
     todoList.splice(indexForRemoval,1);
     renderDisplay();
